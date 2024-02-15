87dfc3e9d74adf9b92c6618734dd293520148c99
VittIvanov <ivanov.vitaly.wdm@mail.ru>
2024-02-15 21:46:26 +0600

    2
    выпадающий список, обработка события

--- src/informMenu/InformMenu.jsx
+++ src/informMenu/InformMenu.jsx
@@ -1,28 +1,72 @@
 import styles from "./InformMenu.module.css"
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 
-const InformMenu = () => {
+const InformMenu = (props) => {
   const [isOpen, setIsOpen] = useState(false) // Состояние для отслеживания состояния списка
+  const dropdownRef = useRef(null);
+
+  // Обработчик события клика вне списка
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    // Функция для обработки события нажатия клавиши "Escape"
+    const handleEscapeKeyPress = (event) => {
+      if (event.key === "Escape") {
+        setIsOpen(false);
+      }
+    }
+
+    // Добавляем обработчики событий при монтировании компонента
+    document.addEventListener("click", handleClickOutside);
+    document.addEventListener("keydown", handleEscapeKeyPress)
+
+    // Удаляем обработчики событий при размонтировании компонента
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+      document.removeEventListener("keydown", handleEscapeKeyPress);
+    }
+
+  }, []);
+
+  // Обработчик клика на ссылку внутри списка
+  const handleLinkClick = (event) => {
+    // Остановка всплытия события, чтобы клик на ссылке не приводил к закрытию списка
+    event.stopPropagation();
+  };
 
   // Функция для переключения состояния списка
   const toggleDropDown = () => {
-    setIsOpen(!isOpen)
-  }
+    setIsOpen(!isOpen);
+  };
+
+  // {`arrow_${isOpen ? 'open' : 'closed'}`}
 
   return (
     <div className={styles.dropdown} onClick={toggleDropDown}>
-      <div className={`arrow_${isOpen ? 'open' : 'closed'}`} />
+      <div ref={dropdownRef} className={styles.arrow} />
       {!isOpen && (<div>
-        <span>доп.информация</span>
+        <span className={styles.inform_list__closed}>доп.информация</span>
       </div>)}
       {isOpen && (
         <ul className={styles.inform_list}>
           {/*  Вставляем содержимое списка, если список открыт */}
-          <li className={styles.inform_list__item}>1 блок</li>
-          <li className={styles.inform_list__item}> 2 блок</li>
-          <li className={styles.inform_list__item}>3 блок</li>
-          <li className={styles.inform_list__item}>4 блок</li>
+          <li className={styles.inform_list__item}>
+            <a href="#" onClick={handleLinkClick}>1 блок</a>
+          </li>
+          <li className={styles.inform_list__item}>
+            <a href="#" onClick={handleLinkClick}>2 блок</a>
+          </li>
+          <li className={styles.inform_list__item}>
+            <a href="#" onClick={handleLinkClick}>3 блок</a>
+          </li>
+          <li className={styles.inform_list__item}>
+            <a href="#" onClick={handleLinkClick}>4 блок</a>
+          </li>
         </ul>
       )}
     </div>
