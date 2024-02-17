7b7dc82eba370129a2ac44f0928702fbf1090bcf
VittIvanov <ivanov.vitaly.wdm@mail.ru>
2024-02-17 16:24:56 +0600

    3
    поправил стили хедера

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,14 +1,12 @@
 import React from "react"
 import styles from "../wrappers/StyledWrapper.module.css"
 import Navigation from "../navigation/Navigation";
-import InformMenu from "../informMenu/InformMenu";
 import RegistrationForm from "../registrationForm/RegistrationForm";
 
 const Header = () => {
   return (
     <header className={styles.styledLayotWrapper}>
       <Navigation />
-      <InformMenu />
       <RegistrationForm />
     </header>
   )
